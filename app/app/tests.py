53635c21c261412e8e5ae49bb861c963b654b1cd
ibrahim keskin <brhmkskn@outlook.com>
2024-04-09 01:06:50 +0200

    Added first test fixed

--- app/app/tests.py
+++ app/app/tests.py
@@ -13,4 +13,4 @@ class CalcTests(SimpleTestCase):
         """Test adding numbers together."""
         res = calc.add(5, 6)
 
-        self.assertEqual(res, 11)        
+        self.assertEqual(res, 11)
