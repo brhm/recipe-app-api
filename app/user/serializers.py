df9c18850b55520ac74529fe923614b75dfbdd7b
ibrahim keskin <brhmkskn@outlook.com>
2024-04-14 21:34:53 +0200

    added manage user token - me

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -22,7 +22,18 @@ class UserSerializer(serializers.ModelSerializer):
         """Create and return a user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
 
-class TokenSerializer(serializers.Serializer):
+    def update(self, instance,validaded_data):
+        """Update and return user."""
+        password=validaded_data.pop('password',None)
+        user=super().update(instance,validaded_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
+class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user auth token."""
     email= serializers.EmailField()
     password= serializers.CharField(
